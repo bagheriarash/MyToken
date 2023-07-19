238ee6f0b38e69fb2594cfff6ceb24de0859f242
bagheriarash <bagheri.arash88@gmail.com>
2023-07-19 01:58:11 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x18c057660ea7bd6905f82c815d614161b56c1d9b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
